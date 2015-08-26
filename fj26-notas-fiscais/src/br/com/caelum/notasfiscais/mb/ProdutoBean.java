ae9e4efabe91ffd446f7cf31e633300ca4740487
dfneto <pdavidfneto@gmail.com>
2015-08-25 23:12:51 -0300

    exercício 3.13

--- fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
+++ fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/ProdutoBean.java
@@ -1,5 +1,7 @@
 package br.com.caelum.notasfiscais.mb;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 
 import br.com.caelum.notasfiscais.dao.ProdutoDao;
@@ -8,16 +10,27 @@ import br.com.caelum.notasfiscais.modelo.Produto;
 @ManagedBean
 public class ProdutoBean {
 	private Produto produto = new Produto();
-
+	private List<Produto> produtos;
+	
 	public Produto getProduto() {
 		return this.produto;
+	}
+	
+	public List<Produto> getProdutos() {
+		if (produtos == null){
+			System.out.println("Carregando produtos ...");
+			produtos = new ProdutoDao().listaTodos();
+		}
+		return produtos;
 	}
 	
 	public void grava() {
 		ProdutoDao dao = new ProdutoDao();
 		dao.adiciona(produto);
 		this.produto = new Produto();
+		this.produtos = dao.listaTodos();
 	}
 
+	
 
 }
