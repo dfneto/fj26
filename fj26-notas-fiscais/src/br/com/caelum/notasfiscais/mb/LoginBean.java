be0709db14ec827655015ab9e65ac83d4263911d
dfneto <pdavidfneto@gmail.com>
2015-09-15 20:51:19 -0300

    exercício 8.9

--- fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/LoginBean.java
+++ fj26-notas-fiscais/src/br/com/caelum/notasfiscais/mb/LoginBean.java
@@ -1,28 +1,30 @@
 package br.com.caelum.notasfiscais.mb;
 
-import java.io.Serializable;
-
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.caelum.notasfiscais.dao.UsuarioDao;
 import br.com.caelum.notasfiscais.modelo.Usuario;
 
-@SuppressWarnings("serial")
 @Named
-@SessionScoped
-public class LoginBean implements Serializable {
+@RequestScoped
+public class LoginBean {
 	private Usuario usuario = new Usuario();
-	
+
+	@Inject
+	private UsuarioLogadoBean usuarioLogado;
+
 	@Inject
 	private UsuarioDao dao;
 
 	public String efetuaLogin() {
 		boolean loginValido = dao.existe(this.usuario);
-		if (loginValido){
+		if (loginValido) {
+			usuarioLogado.logar(usuario);
 			return "produto?faces-redirect=true";
 		} else {
+			usuarioLogado.deslogar();
 			this.usuario = new Usuario();
 			return "login";
 		}
